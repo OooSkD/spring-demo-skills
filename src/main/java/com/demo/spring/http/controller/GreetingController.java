50e58a9918d53a0587ebe59c13e9e811d7097844
OooSkD <OooSkD@users.noreply.github.com>
2023-09-13 14:11:37 +0300

    version 0.9
    added a simple web controller

---
+++ src/main/java/com/demo/spring/http/controller/GreetingController.java
@@ -0,0 +1,29 @@
+package com.demo.spring.http.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/api/v1")
+public class GreetingController {
+
+    @GetMapping( "/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/bye")
+    public ModelAndView bye() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("greeting/bye");
+
+        return modelAndView;
+    }
+}
